131f7ca2f58b8b074e040bc7ee2b1ed720e006a4
Muhammad Chamdani Sukron <scrum@yatimmandiri.org>
2024-02-27 17:38:00 +0700

    Update Kernel
    Update Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,10 +43,10 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            JsonMiddleware::class,
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            JsonMiddleware::class,
         ],
     ];
 
